5c83744dc40fafca1fad727cfc2af85a44361b77
hoshico <melancholinista.yasu@gmail.com>
2022-03-14 08:24:40 +0900

    installdaisyui

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className=''>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button　className='inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900'>情報取得</button>
         <a
           className="App-link"
           href="https://reactjs.org"
